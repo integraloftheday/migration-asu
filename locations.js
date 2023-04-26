54a578e9d73022425d33725bd41c639716b4c550
Mason Manetta <mmanetta@asu.edu>
2023-04-25 18:44:08 -0700

    push

--- locations.js
+++ locations.js
@@ -2,11 +2,39 @@ var geoData ={
     "locations":
     [
         {
-            name:"Hayden",
-            lat:33.4194533,
-            long:-111.9348256,
-            link:"google.com",
-            text:"Hayden Library was founded in 1884"
+            name:"Old Main (Normal School)",
+            lat:33.421377,
+            long:-111.9339594,
+            link:"https://app.pictarize.com/p?projectId=64370dac7cd1930011a93f8b&buildId=6448818e94edd50011f7889e",
+            text:"Old Main (Normal School)"
+        },
+        {
+            name:"The Old Church",
+            lat: 33.4216791,
+            long:-111.9346938,
+            link:"https://app.pictarize.com/p?projectId=64487e0f94edd50011f78898&buildId=6448807f94edd50011f7889b",
+            text:"The Old Church"
+        },
+        {
+            name:"Hayden Library",
+            lat:33.4185773,
+            long:-111.9355418,
+            link:"https://app.pictarize.com/p?projectId=6437083d7cd1930011a93f84&buildId=6448813f94edd50011f7889d",
+            text:"Hayden Library"
+        },
+        {
+            name:"Chicano Bush on Hayden Lawn",
+            lat:33.4190633,
+            long:-111.9352481,
+            link:"https://app.pictarize.com/p?projectId=64487abd94edd50011f7888f&buildId=644880e794edd50011f7889c",
+            text:"Chicano Bush"
+        },
+        {
+            name:"Barrett",
+            lat:33.4159028,
+            long:-111.9285464,
+            link:"",
+            text:"Barrett"
         }
     ]
 };
